f359d9ca4981a1ccdc0f999c3411989f45ca9a0b
Daniel Bovensiepen Li <oss@bovi.li>
2022-12-03 22:58:59 +0000

    add parsing of devcontainer and first test for it with CI

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,48 @@
 /* CLI program to manage devcontainers */
 
-use std::env;
+mod susi {
+    use std::{fs::File, io::Read};
+    use serde_json::Value;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    /* check if the user has passed any arguments */
-    if args.len() == 1 {
-        println!("No arguments passed");
-        std::process::exit(0);
+
+    // define public struct devcontaienr with public fields name and image
+    pub struct Devcontainer {
+        pub name: String,
+        pub image: String,
     }
-    let command = &args[1];
 
-    match command.as_str() {
-        "start" => start(),
-        _ => println!("Unknown command"),
+    pub fn parse_devcontainer(filename: &str) -> Devcontainer {
+        let mut file = File::open(filename).expect("file not found");
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).expect("something went wrong reading the file");
+
+        /* we need to get rid of comments in the json */
+        let re = regex::Regex::new(r"//.*\r?\n").unwrap();
+        let result = re.replace_all(&contents, "");
+
+        let json: Value = serde_json::from_str(&result).unwrap();
+        let image = json["image"].as_str().unwrap();
+        let name = json["name"].as_str().unwrap();
+
+        Devcontainer {
+            name: name.to_string(),
+            image: image.to_string(),
+        }
     }
 }
 
-fn start() {
-    println!("Starting devcontainer");
+fn main() {
+    println!("susi");
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::susi;
+
+    #[test]
+    fn parse_simple_devcontainer() {
+        let result = susi::parse_devcontainer("test/devcontainer.simple.json");
+        assert_eq!(result.name, "Rust");
+        assert_eq!(result.image, "mcr.microsoft.com/devcontainers/rust:1-bullseye");
+    }
 }
