8f3970940744467d6fb10ef230ec69aad91604ab
Daniel Bovensiepen Li <oss@bovi.li>
2022-12-03 23:35:46 +0000

    catch workspace folder without devcontainer.json file

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ mod susi {
     use std::{fs::File, io::Read};
     use serde_json::Value;
 
-
-    // define public struct devcontaienr with public fields name and image
     pub struct Devcontainer {
         pub name: String,
         pub image: String,
@@ -30,10 +28,6 @@ mod susi {
         }
     }
 
-    // identify devcontainer in the folder structure of the passed workspace folder
-    // search in the following order:
-    // 1. .devcontainer/devcontainer.json
-    // and return the parse_devcontainer result
     pub fn identify_devcontainer(workspace_folder: &str) -> Devcontainer {
         let devcontainer_path = format!("{}/.devcontainer/devcontainer.json", workspace_folder);
         let devcontainer_path_root = format!("{}/devcontainer.json", workspace_folder);
@@ -44,26 +38,29 @@ mod susi {
             return parse_devcontainer(&devcontainer_path_root);
         } else {
             let devcontainer_path_sub = format!("{}/.devcontainer", workspace_folder);
-            for entry in std::fs::read_dir(devcontainer_path_sub).unwrap() {
-                let entry = entry.unwrap();
-                let path = entry.path();
-                if path.is_dir() {
-                    let devcontainer_path = format!("{}/devcontainer.json", path.display());
-                    if std::path::Path::new(&devcontainer_path).exists() {
-                        return parse_devcontainer(&devcontainer_path);
+            // check if .devcontainer folder exists
+            if std::path::Path::new(&devcontainer_path_sub).exists() {
+                for entry in std::fs::read_dir(devcontainer_path_sub).unwrap() {
+                    let entry = entry.unwrap();
+                    let path = entry.path();
+                    if path.is_dir() {
+                        let devcontainer_path = format!("{}/devcontainer.json", path.display());
+                        if std::path::Path::new(&devcontainer_path).exists() {
+                            return parse_devcontainer(&devcontainer_path);
+                        } else {
+                            continue;
+                        }
                     } else {
                         continue;
                     }
-                } else {
-                    continue;
                 }
             }
         }
 
-        // if no devcontainer.json was found, return empty Devcontainer
+        /* no devcontainer.json file found, let's use a default one */
         Devcontainer {
-            name: String::new(),
-            image: String::new(),
+            name: "Alpine".to_string(),
+            image: "mcr.microsoft.com/devcontainers/base:alpine-3.16".to_string(),
         }
     }
 }
@@ -106,5 +103,12 @@ mod tests {
         let result_baseline = susi::parse_devcontainer("test/workspaces/complex2/.devcontainer/jfdasjfh/devcontainer.json");
         assert_eq!(result_identify.name, result_baseline.name);
         assert_eq!(result_identify.image, result_baseline.image);
+    }
+
+    #[test]
+    fn identify_devcontainer_empty() {
+        let result_identify = susi::identify_devcontainer("test/workspaces/empty");
+        assert_eq!(result_identify.name, "Alpine");
+        assert_eq!(result_identify.image, "mcr.microsoft.com/devcontainers/base:alpine-3.16");
     }
 }
