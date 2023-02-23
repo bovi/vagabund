297a3ee60451eb281ef102098b51b24fdd60e4ee
Daniel Bovensiepen Li <oss@bovi.li>
2023-02-24 01:01:13 +0800

    add basic qemu test and devcontainer

--- lib/susi.rb
+++ lib/susi.rb
@@ -1,3 +1,23 @@
-class Susi
-  # TODO
+class QEMU
+  def self.create_img(size_in_g: 40, path: nil)
+    result = `qemu-img create -f qcow2 #{path} #{size_in_g}G`
+    raise "Failed to create disk image: #{result}" unless $?.success?
+  end
+
+  def self.create_vm(name: nil, img_path: nil, ram: 1024, cpu: 1, vnc: nil, boot: nil)
+    raise "Name is required" if name.nil?
+    raise "Image path is required" if img_path.nil?
+    raise "VNC port is required" if vnc.nil?
+
+    result = `qemu-system-x86_64 -name #{name} -m #{ram} -smp #{cpu} -hda #{img_path} -vnc :#{vnc} -boot #{boot}`
+    raise "Failed to create VM: #{result}" unless $?.success?
+  end
+end
+
+module Susi
+  class CLI
+    def self.start(argv)
+      QEMU.create_img(size_in_g: 40, path: "test.img")
+    end
+  end
 end
---
+++ test/qemu_test.rb
@@ -0,0 +1,25 @@
+require 'test/unit'
+require 'tempfile'
+require_relative '../lib/susi'
+
+class QEMU_Test < Test::Unit::TestCase
+  def test_create_img
+    # create a temporary filename
+    file = Tempfile.new('qemu_test_img').path
+    size = 40
+
+    QEMU.create_img(size_in_g: size, path: file)
+
+    assert File.exist?(file)
+    assert_equal "qcow2", `qemu-img info #{file} | grep "file format" | awk '{print $3}'`.strip
+    assert_equal size, `qemu-img info #{file} | grep "virtual size" | awk '{print $3}'`.strip.to_i
+
+    File.delete(file)
+  end
+
+  #def test_create_vm
+  #  assert_raise RuntimeError do
+  #    QEMU.create_vm(name: nil, img_path: nil, ram: 1024, cpu: 1, vnc: nil, boot: nil)
+  #  end
+  #end
+end
