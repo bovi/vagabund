e3e10a0c07abd553a5c421a2c0c2ef0652dcdaff
Daniel Bovensiepen Li <oss@bovi.li>
2023-02-24 01:35:52 +0800

    redirect errors to stdout

--- Rakefile
+++ Rakefile
@@ -1,7 +1,7 @@
-# a test task
 task :test do
-  # execute all tests in test folder
   Dir.glob("test/**/*_test.rb").each do |file|
     require_relative file
   end
 end
+
+task default: :test
--- lib/susi.rb
+++ lib/susi.rb
@@ -1,6 +1,6 @@
 class QEMU
   def self.create_img(size_in_g: 40, path: nil)
-    result = `qemu-img create -f qcow2 #{path} #{size_in_g}G`
+    result = `qemu-img create -f qcow2 #{path} #{size_in_g}G 2>&1`
     raise "Failed to create disk image: #{result}" unless $?.success?
   end
 
@@ -9,7 +9,7 @@ class QEMU
     raise "Image path is required" if img_path.nil?
     raise "VNC port is required" if vnc.nil?
 
-    result = `qemu-system-x86_64 -name #{name} -m #{ram} -smp #{cpu} -hda #{img_path} -vnc :#{vnc} -boot #{boot}`
+    result = `qemu-system-x86_64 -name #{name} -m #{ram} -smp #{cpu} -hda #{img_path} -vnc :#{vnc} -boot #{boot} 2>&1`
     raise "Failed to create VM: #{result}" unless $?.success?
   end
 end
