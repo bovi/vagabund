ff3928e4b1e746597bf23a89a5366311ab946325
Daniel Bovensiepen Li <oss@bovi.li>
2023-02-27 23:43:16 +0800

    add option parser

--- lib/susi.rb
+++ lib/susi.rb
@@ -1,19 +1,39 @@
 require_relative "qemu.rb"
+require 'optparse'
 
 module Susi
   # handling the command line interface
   class CLI
     def self.start(argv)
-      # parse arguments from command line for create image and create vm
-      argv.each do |arg|
-        case arg
-        when "--create-img"
-          Susi::QEMU.create_img(size_in_g: 40, path: "test.img")
-        when "--create-vm"
-          Susi::QEMU.new(name: "test", img_path: "test.img", ram: 1024, cpu: 1, vnc: 0, boot: "c")
+      oparser = OptionParser.new do |opts|
+        opts.on '-n', '--name=NAME', 'Set name for VM'
+        opts.on '-v', '--version', 'Print version'
+        opts.on '-c', '--create-img', 'Create QCOW2 Image'
+        opts.on '-s', '--start-vm', 'Start VM'
+        opts.on '-f', '--file=FILE', 'File name for this command'
+        opts.on '--size=SIZE', 'Size of the image file'
+      end
+
+      options = {}
+      oparser.parse!(into: options)
+      puts options
+
+      o = -> (opt) {
+        if options.has_key? opt
+          options[opt]
+        else
+          raise ArgumentError, "Argument '#{opt}' is missing."
         end
+      }
+
+      case options
+      in "start-vm": true
+        Susi::QEMU.new(name: o.(:name), img: o.(:file), ram: 1024, cpu: 1, vnc: 0, boot: "c")
+      in "create-img": true
+        Susi::QEMU.create_img(size: o.(:size), path: o.(:file))
+      else
+        puts 'unrecognized'
       end
     end
   end
-
 end
