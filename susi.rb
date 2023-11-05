67be79c6e8c457c7aebd3324386b9902489af3cb
Daniel Bovensiepen <oss@bovi.li>
2023-11-05 21:08:02 +0800

    feat: add kvm, fix net bug, improve init

--- susi.rb
+++ susi.rb
@@ -3,6 +3,18 @@
 require 'optparse'
 require 'socket'
 require 'json'
+require 'net/ssh'
+require 'net/scp'
+
+SSH_PORT = 7022
+VNC_PORT = 7090 - 5900
+QMP_PORT = 7044
+
+SUSI_HOME = "#{Dir.home}/.susi"
+SUSI_PWD = "#{Dir.pwd}/.susi"
+DEFAULT_QCOW = "#{SUSI_HOME}/default.qcow2"
+CONFIG = "#{SUSI_PWD}/config.json"
+DISK = "#{SUSI_PWD}/disk.qcow2"
 
 unless system("which qemu-system-x86_64 > /dev/null")
   puts "Please install qemu"
@@ -34,7 +46,7 @@ def list_vms
     if id
       puts
       puts "ID: " + id[1]
-      vnc_port = line.match(/-vnc\s0.0.0.0:(\d+)/)
+      vnc_port = line.match(/-vnc\slocalhost:(\d+)/)
       if vnc_port
         puts "\tvnc://localhost:#{vnc_port[1].to_i + 5900} "
       end
@@ -58,15 +70,56 @@ def list_vms
   end
 end
 
-def ssh_vm
-  system("ssh -p #{SSH_PORT} susi@localhost")
+def c(key)
+  raise "No .susi config" if !File.exist?(CONFIG)
+  config = JSON.parse(File.read(CONFIG))
+  config[key]
 end
 
-SSH_PORT = 7022
-VNC_PORT = 7090 - 5900
-QMP_PORT = 7044
+def ssh_vm
+  user = c('user')
+  system("ssh -p #{SSH_PORT} #{user}@localhost")
+end
 
-DEFAULT_QCOW = "#{Dir.home}/.susi/default.qcow2"
+def init_vm
+  raise 'project already initialized' if File.exist?(CONFIG)
+
+  pwd = Dir.pwd
+  Dir.mkdir(SUSI_PWD)
+
+  unique_id = (0...8).map { (65 + rand(26)).chr }.join
+
+  config = {}
+  config['id'] = unique_id
+  config['user'] = 'susi'
+  config['ram'] = 8
+
+  File.open(CONFIG, "w") do |f|
+    f.write(config.to_json)
+  end
+
+  clone_qcow2(DEFAULT_QCOW, DISK)
+
+  start_vm(DISK)
+
+  # login with password to the VM via net-ssh
+  Net::SSH.start("localhost", "susi",
+                  :port => SSH_PORT, :password => "susi") do |ssh|
+    # copy public key
+    ssh.exec!("mkdir -p .ssh")
+    ssh.exec!("chmod 700 .ssh")
+    ssh.exec!("touch .ssh/authorized_keys")
+    ssh.scp.upload!("#{Dir.home}/.ssh/id_ed25519.pub", ".ssh/authorized_keys")
+    ssh.exec!("chmod 600 .ssh/authorized_keys")
+    # copy private key
+    ssh.scp.upload!("#{Dir.home}/.ssh/id_ed25519", ".ssh/id_ed25519")
+    ssh.exec!("chmod 600 .ssh/id_ed25519")
+
+    # deactivate ssh password authentication
+    ssh.exec!("sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config")
+    ssh.exec!("sudo systemctl restart sshd")
+  end
+end
 
 def vm_running?
   begin
@@ -92,9 +145,8 @@ def start_vm(image_name, installer = nil)
 
   # name configuration
   folder = File.dirname(image_name)
-  if File.exist?("#{folder}/id")
-    id = File.read("#{folder}/id")
-    args << "-name #{id}"
+  if File.exist?(CONFIG)
+    args << "-name #{c('id')}"
   end
 
   # network configuration
@@ -105,15 +157,22 @@ def start_vm(image_name, installer = nil)
   net << ",hostfwd=tcp::#{SSH_PORT}-:22"
   net << ",hostfwd=tcp::7080-:80"
   net << ",hostfwd=tcp::7443-:443"
+  args << net.join
 
   args << "-qmp tcp:localhost:#{QMP_PORT},server,nowait"
-  args << "-vnc 0.0.0.0:#{VNC_PORT},password=on"
+  args << "-vnc localhost:#{VNC_PORT},password=on"
 
   # other configuration
   args << "-m 8G"
   args << "-display none"
   args << "-rtc base=localtime"
   args << "-daemonize"
+
+  if kvm_available?
+    args << "-enable-kvm"
+  else
+    puts "KVM is not available. Running in emulation mode."
+  end
 
   cmd = "qemu-system-x86_64 #{args.join(" ")}"
   puts cmd
@@ -171,6 +230,24 @@ def quit_vm
   socket.close
 end
 
+def powerdown_vm
+  socket = TCPSocket.new("localhost", QMP_PORT)
+  socket.gets
+  socket.puts('{"execute":"qmp_capabilities"}')
+  response = JSON.parse(socket.gets)
+  if response["return"] == {}
+    puts "QMP connection established"
+  else
+    puts "QMP connection failed"
+  end
+  socket.puts('{"execute":"system_powerdown"}')
+  socket.close
+end
+
+def kvm_available?
+  File.exist?('/dev/kvm') && File.readable?('/dev/kvm') && File.writable?('/dev/kvm')
+end
+
 # parse command line arguments
 # if arguments is susi.rb -c -n <image_name> -s <size>
 # then the following code will parse the arguments
@@ -222,6 +299,10 @@ OptionParser.new do |opts|
   opts.on("-s", "--ssh", "SSH into the VM") do |ssh|
     options[:ssh] = ssh
   end
+
+  opts.on("-d", "--powerdown", "Power down the VM") do |halt|
+    options[:halt] = halt
+  end
 end.parse!
 
 # check if the user has provided all the required arguments
@@ -233,34 +314,27 @@ elsif options[:run] && options[:name]
   start_vm(options[:name], options[:installer])
 elsif options[:quit]
   quit_vm
+elsif options[:halt]
+  powerdown_vm
 elsif options[:init]
-  if Dir.exist?("#{Dir.pwd}/.susi")
+  if Dir.exist?(SUSI_PWD)
     puts "VM already initialized"
   else
     puts "Initializing the VM"
-
-    pwd = Dir.pwd
-    Dir.mkdir("#{pwd}/.susi")
-
-    unique_id = (0...8).map { (65 + rand(26)).chr }.join
-    File.open("#{pwd}/.susi/id", "w") do |f|
-      f.write(unique_id)
-    end
-
-    clone_qcow2(DEFAULT_QCOW, "#{pwd}/.susi/disk.qcow2")
+    init_vm
   end
 elsif options[:list]
   list_vms
 elsif options[:ssh]
   ssh_vm
 else
-  if Dir.exist?("#{Dir.pwd}/.susi")
-    if File.exist?("#{Dir.pwd}/.susi/disk.qcow2")
+  if Dir.exist?(SUSI_PWD)
+    if File.exist?(DISK)
       if vm_running?
         puts "VM is running"
       else
         # start VM
-        start_vm("#{Dir.pwd}/.susi/disk.qcow2")
+        start_vm(DISK)
       end
     end
   else
