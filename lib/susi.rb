029e98bc647a07e29445bb24fe1778c99cbe78d0
Daniel Bovensiepen <oss@bovi.li>
2023-11-05 23:39:20 +0800

    doc: add susi command documentation

--- lib/susi.rb
+++ lib/susi.rb
@@ -22,14 +22,28 @@ def execute_program(options)
     $verbose = false
   end
 
+  # cmd: susi -c -n <name> -s <size>
+  # desc: create a new image with the given name and size
   if options[:create] && options[:name] && options[:size]
     create_qcow2(options[:name], options[:size])
+  
+  # cmd: susi -r -n <name>
+  # desc: run the VM with the given image name
   elsif options[:run] && options[:name]
     start_vm(options[:name], options[:installer])
+
+  # cmd: susi quit
+  # desc: force quit the VM
   elsif options[:quit]
     quit_vm
-  elsif options[:halt] or ARGV[0] == "stop"
+  
+  # cmd: susi stop
+  # desc: stop the VM
+  elsif options[:halt] or ARGV[0] == "stop" or ARGV[0] == "halt"
     powerdown_vm
+  
+  # cmd: susi init
+  # desc: initialize the VM configuration and disk
   elsif options[:init] or ARGV[0] == "init"
     if Dir.exist?(SUSI_PWD)
       raise "VM already initialized"
@@ -37,18 +51,32 @@ def execute_program(options)
       log "Initializing the VM"
       init_vm
     end
+  
+  # cmd: susi (ls|list)
+  # desc: list all the VMs
   elsif options[:list] or ARGV[0] == "list" or ARGV[0] == "ls"
     list_vms
+  
+  # cmd: susi ssh
+  # desc: ssh into the VM
   elsif options[:ssh] or ARGV[0] == "ssh"
     ssh_vm
+
+  # cmd: susi rm
+  # desc: remove the VM disk and the config
   elsif ARGV[0] == "rm"
     quit_vm
-    # delete susi folder
     FileUtils.rm_rf(SUSI_PWD)
+  
+  # cmd: susi clean
+  # desc: remove the VM disk but keep the config
   elsif ARGV[0] == "clean"
     quit_vm
     # delete disk
     FileUtils.rm_rf(DISK)
+  
+  # cmd: susi start
+  # desc: start the VM (clone disk if not exists) from config
   elsif ARGV[0] == "start"
     if config_exists?
       if disk_exists?
@@ -64,6 +92,8 @@ def execute_program(options)
     else
       raise "Please initialize the VM first"
     end
+  
+  # HELP
   else
     log "Please provide all the required arguments"
     log "Run susi.rb -h for help"
